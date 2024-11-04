26f5406ba1331911284354eea555cf01985314d6
vres271 <vres271@gmail.com>
2024-11-04 16:11:09 +0400

    sosna

--- src/app/ui/button/button.component.ts
+++ src/app/ui/button/button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { UIEvent, UIRole } from '../types';
 
 @Component({
