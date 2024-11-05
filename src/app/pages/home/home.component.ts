f67999caa2c1c387075aea07e302dd8254eb311d
vres271 <vres271@gmail.com>
2024-11-05 23:23:25 +0400

    Tlka zbs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,17 @@ import { ButtonComponent } from '../../ui/button/button.component';
   standalone: true,
   imports: [ButtonComponent],
   templateUrl: './home.component.html',
+  styles: `
+    .sosna-logo {
+      background: url("/favicon.svg");
+      height: 100%;
+      width: 100%;
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+  `
 })
 export class HomeComponent {
 
