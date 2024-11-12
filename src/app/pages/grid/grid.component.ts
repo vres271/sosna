5a1d1b8fd487e80337dae62d4503f794dc314bcc
vres271 <vres271@gmail.com>
2024-11-13 00:55:24 +0400

    test send color to esp

--- src/app/pages/grid/grid.component.ts
+++ src/app/pages/grid/grid.component.ts
@@ -16,8 +16,23 @@ export class GridComponent {
     this.color = e.target.value;
   }
 
-  clickCell(cell: {color: string}) {
+  clickCell(cell: {color: string}, i: number) {
     cell.color = this.color;
+    console.log(      parseInt('0x'+cell.color[1]+cell.color[2]),
+    parseInt('0x'+cell.color[3]+cell.color[4]),
+    parseInt('0x'+cell.color[5]+cell.color[6]),
+
+)
+    fetch('http://192.168.0.103/get?led='+i+
+      '&h='+parseInt('0x'+cell.color[1]+cell.color[2])+
+      '&s='+parseInt('0x'+cell.color[3]+cell.color[4])+
+      '&v='+parseInt('0x'+cell.color[5]+cell.color[6])+
+      '')
+      .then(res => res.json())
+      .then(obj => console.log(obj))
   }
+
+
+
 
 }
