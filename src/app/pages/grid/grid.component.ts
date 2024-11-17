fc288fde2b4e948c57c98a7e289d69d36eb69783
vres271 <vres271@gmail.com>
2024-11-17 14:06:47 +0400

    Test sending POST set leds requiest

--- src/app/pages/grid/grid.component.ts
+++ src/app/pages/grid/grid.component.ts
@@ -17,14 +17,47 @@ export class GridComponent {
   }
 
   clickCell(cell: {color: string}, i: number) {
+
     cell.color = this.color;
-    fetch('http://192.168.0.103/get?led='+i+
-      '&r='+parseInt('0x'+cell.color[1]+cell.color[2])+
-      '&g='+parseInt('0x'+cell.color[3]+cell.color[4])+
-      '&b='+parseInt('0x'+cell.color[5]+cell.color[6])+
-      '')
+
+    // fetch('http://192.168.0.103/get?led='+i+
+    //   '&r='+parseInt('0x'+cell.color[1]+cell.color[2])+
+    //   '&g='+parseInt('0x'+cell.color[3]+cell.color[4])+
+    //   '&b='+parseInt('0x'+cell.color[5]+cell.color[6])+
+    //   '')
+    //   .then(res => res.json())
+    //   .then(obj => console.log(obj))
+
+    const formData  = new FormData();
+    // formData.append('led', '' + i);
+    // formData.append('r', '' + parseInt('0x'+cell.color[1]+cell.color[2]));
+    // formData.append('g', '' + parseInt('0x'+cell.color[3]+cell.color[4]));
+    // formData.append('b', '' + parseInt('0x'+cell.color[5]+cell.color[6]));
+    const [r,g,b] = [
+      parseInt('0x'+cell.color[1]+cell.color[2]),
+      parseInt('0x'+cell.color[3]+cell.color[4]),
+      parseInt('0x'+cell.color[5]+cell.color[6]),    
+    ]
+
+    const ledsData = [
+      `${i}:${r},${g},${b},1000,1,1|${r},${g-150},${b-150},2000,1,1|${r-150},${g},${b},2000,1,1;`,
+      `${i+1}:${r-110},${g-120},${b},1500,1,1|${r},${g-140},${b-130},3000,1,1|${r-130},${g},${b},4000,1,1;`,
+      `${i+2}:255,0,0,1000,1,1|0,255,0,2000,1,1|0,0,255,3000,1,1`,
+      // `${i}:${r},${g-50},${b-50},2000,1,1;`,
+      // `${i}:${r-50},${g},${b},2000,1,1;`,
+    ]
+
+    formData.append('payload', ledsData.join(''));
+
+    fetch('http://192.168.0.103/set', {
+      method: 'POST',
+      body: formData,
+    })
       .then(res => res.json())
       .then(obj => console.log(obj))
+
+
+
   }
 
   onMouseEnter(cell: {color: string}, i: number, e: MouseEvent) {
