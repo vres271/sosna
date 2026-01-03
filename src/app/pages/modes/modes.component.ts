08337c2956b3d550d45cc581659c7e9242ba48ae
vres271 <vres271@gmail.com>
2026-01-04 00:58:03 +0400

    modes no load

--- src/app/pages/modes/modes.component.ts
+++ src/app/pages/modes/modes.component.ts
@@ -6,11 +6,28 @@ export interface IMode {
   name: string;
 }
 
-// export const modes: IMode[] = [
-//   {id: 1, name: 'Sinus Warm'},
-//   {id: 2, name: 'Sinus Cold'},
-//   {id: 3, name: 'Sinus RGB'},
-// ]
+export const modes: IMode[] = [
+  // {id: 1, name: 'Sinus Warm'},
+  // {id: 2, name: 'Sinus Cold'},
+  // {id: 3, name: 'Sinus RGB'},
+
+  {id: 1, name: 'sinus1'},
+  {id: 2, name: 'sinus2'},
+  {id: 3, name: 'sinus3'},
+  {id: 4, name: 'purpleWave'},
+  {id: 5, name: 'cyanRush'},
+  {id: 6, name: 'sunsetGlow'},
+  {id: 7, name: 'softPink'},
+  {id: 8, name: 'emeraldGreen'},
+  {id: 9, name: 'rainbowFrenzy'},
+  {id: 10, name: 'lavenderDawn'},
+  {id: 11, name: 'icyBlue'},
+  {id: 12, name: 'magicalMagenta'},
+  {id: 13, name: 'neonShock'},
+  {id: 14, name: 'forestMoss'},
+  {id: 15, name: 'flowerRomance'},
+
+]
 
 @Component({
   selector: 'app-modes',
@@ -34,15 +51,18 @@ export class ModesComponent implements OnInit{
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.ledsService.getModes().then(res => {
-      if (res.result) {
-        this.modes = res.result?.modes;
-        const mode = res.result?.mode;
-        this.selectedMode = this.modes.find(m => m.id === mode) || null;
-        this.isLoading = false;
-        this.cd.detectChanges();
-      }
-    })
+    // this.ledsService.getModes().then(res => {
+    const res = {
+      result: { modes, mode: 1 }
+    }
+    if (res.result) {
+      this.modes = res.result?.modes;
+      const mode = res.result?.mode;
+      this.selectedMode = this.modes.find(m => m.id === mode) || null;
+      this.isLoading = false;
+      this.cd.detectChanges();
+    }
+    // })
   }
 
   setMode(mode: IMode | null) {
